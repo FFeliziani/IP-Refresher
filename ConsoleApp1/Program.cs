8bbc1dc357180c260f00c743905b5d2155801be1
Federico Feliziani <me@federicofeliziani.com>
2017-04-19 08:19:44 +0200

    First Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using Ovh.Api;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace api_tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(!File.Exists("./ovh.conf"))
+            {
+                throw new IOException();
+            }
+            Environment.SetEnvironmentVariable("OVH_ENDPOINT", "ovh-eu");
+            Environment.SetEnvironmentVariable("OVH_APPLICATION_KEY", "J1MXVC7BkTJzoTi4");
+            Environment.SetEnvironmentVariable("OVH_APPLICATION_SECRET", "q3DUZlTvv0Eag01dML1LLo7KapYAj7rQ");
+            Environment.SetEnvironmentVariable("OVH_CONSUMER_KEY", "oYkmfjAambiOUqCwMx8me5En4lgBBtfq");
+            Client client = new Client();
+            CredentialRequest requestPayload = new CredentialRequest(
+                new List<AccessRight>(){
+    new AccessRight("GET", "/*"),
+    new AccessRight("PUT", "/*"),
+    new AccessRight("POST", "/*"),
+    new AccessRight("DELETE", "/*"),
+                }, "http://www.google.com"
+            );
+
+            CredentialRequestResult credentialRequestResult =
+                client.RequestConsumerKey(requestPayload);
+            Console.Write(
+                String.Format("Please visit {0} to authenticate ",
+                    credentialRequestResult.ValidationUrl));
+            Console.WriteLine("and press enter to continue");
+            Console.ReadLine();
+
+            PartialMe me = client.Get<PartialMe>("/me");
+
+            Console.WriteLine(
+                String.Format("Welcome, {0}", me.firstname));
+            Console.WriteLine(
+                String.Format("Btw, your 'consumerKey' is {0}",
+                    credentialRequestResult.ConsumerKey));
+            Console.ReadLine();
+        }
+    }
+
+    class PartialMe
+    {
+        public string firstname { get; set; }
+        public string name { get; set; }
+    }
+}
--- IP Refresher/Program.cs
+++ IP Refresher/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Net;
+using OVHApi;
+using System.Threading;
 
 namespace IP_Refresher
 {
@@ -10,6 +11,72 @@ namespace IP_Refresher
     {
         static void Main(string[] args)
         {
+            while(true)
+            {
+                //Check 8.8.8.8 or 8.8.4.4
+                var isInternetUp = checkIPUp(@"8.8.8.8");
+                if (!isInternetUp)
+                {
+                    isInternetUp = checkIPUp(@"8.8.4.4");
+                }
+                if (isInternetUp)
+                {
+                    //Get current ip address
+                    var currentIP = callRemoteAPI("https://api.ipify.org");
+                    //Get OVH ip address
+                    var ovhIP = callOVHAsync().Result;
+                    //if different, update.
+                    if (currentIP != ovhIP)
+                    {
+                        postOVHAsync(currentIP);
+                    }
+                    Console.Read();
+                }
+                Thread.Sleep(300000);
+            }
+        }
+
+        private static Boolean checkIPUp(String ipAddress)
+        {
+            bool isPingable = false;
+            Ping pinger = new Ping();
+            try
+            {
+                PingReply reply = pinger.Send(ipAddress);
+                isPingable = reply.Status == IPStatus.Success;
+            }
+            catch (PingException)
+            {
+
+            }
+            return isPingable;
+        }
+
+        private static String callRemoteAPI(String url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(new Uri(url));
+        }
+
+        private static async Task<string> callOVHAsync()
+        {
+
+            //Environment.SetEnvironmentVariable("OVH_ENDPOINT", "ovh-eu");
+            //Environment.SetEnvironmentVariable("OVH_APPLICATION_KEY", "J1MXVC7BkTJzoTi4");
+            //Environment.SetEnvironmentVariable("OVH_APPLICATION_SECRET", "q3DUZlTvv0Eag01dML1LLo7KapYAj7rQ");
+            //Environment.SetEnvironmentVariable("OVH_CONSUMER_KEY", "s1SL3ItS4noJTfWKo8gmRpb9wwVAi6VR");
+            OvhApiClient api = new OvhApiClient("J1MXVC7BkTJzoTi4", "q3DUZlTvv0Eag01dML1LLo7KapYAj7rQ", OvhInfra.Europe, "s1SL3ItS4noJTfWKo8gmRpb9wwVAi6VR");
+            OvhApi.Models.Domain.Zone.Record r = await api.GetDomainZoneRecord("legagladio.it", 1445634879);
+            return r.Target;
+        }
+
+
+        private static async void postOVHAsync(string currentIP)
+        {
+            OvhApiClient api = new OvhApiClient("J1MXVC7BkTJzoTi4", "q3DUZlTvv0Eag01dML1LLo7KapYAj7rQ", OvhInfra.Europe, "s1SL3ItS4noJTfWKo8gmRpb9wwVAi6VR");
+            OvhApi.Models.Domain.Zone.Record r = await api.GetDomainZoneRecord("legagladio.it", 1445634879);
+            r.Target = currentIP;
+            await api.UpdateDomainZoneRecord(r, "legagladio.it", 1445634879);
         }
     }
 }
